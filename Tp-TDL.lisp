73f07d3535728ac2ad22eae5771271c9131359e4
Celes Gomez <fedgomez@fi.uba.ar>
2024-06-04 18:20:18 -0300

    agrego un nivel de local

--- Tp-TDL.lisp
+++ Tp-TDL.lisp
@@ -12,14 +12,39 @@
     ;; Imprimir en pantalla usando Browse
     ((eq (car exp) 'Browse)
      (let ((val (cadr exp)))
-       (if (symbolp val) ; Verifica si es una variable
-           (print (gethash val env))
-           (print val)) ; Si no, imprime el valor directamente
+        (if (symbolp val) ; Verifica si es una variable
+          (if (boundp '*local-env*) ;; si esta el entorno local 
+            (print (gethash val *local-env*)) ;; Imprimo el valor de la variable en el local
+            (print (gethash val env))
+          )
+          (print val)
+        ); Si no, imprime el valor directamente
        nil))
 
     ;; Otras ramas de la función eval-oz...
+    (
+      (eq (car exp) 'local)
+      (let ((val (cadr exp)))
+           (defvar *local-env* (make-hash-table :test 'equal))
+      )
+    )
 
-    (t (error "Expresión no reconocida: ~a" exp))))
+    (
+      (eq (cadr exp) '=)
+      (let ((var (car exp))
+           (val (caddr exp))) 
+           (setf (gethash var *local-env*) val)
+      )
+    )
+
+    (
+      (eq (car exp) 'end)
+      (makunbound '*local-env*)
+    )
+
+    (t (error "Expresión no reconocida: ~a" exp)))
+)
 
 (defun run-oz (program)
-  (mapcar (lambda (exp) (evaluar-oz exp *global-env*)) program))
+  (mapcar (lambda (exp) (evaluar-oz exp *global-env*)) program)
+)
--- main.lisp
+++ main.lisp
@@ -7,3 +7,11 @@
  '((declare x 20)
    (Browse x)))
 
+(
+  run-oz '((local A in) (A = 35) (Browse A) (end))
+)
+
+(
+  run-oz '((local A in) (A = 35) (Browse A) (end) (Browse A))
+  ;; pruebo un browse de A fuera del local pq aca deberia no tener valor
+)
