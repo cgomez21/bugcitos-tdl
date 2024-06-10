e22e648147b4f968d1547dbcff2812c0c15f318f
Celes Gomez <fedgomez@fi.uba.ar>
2024-06-10 18:42:18 -0300

    Agrego varios locals y errores de scoping

--- Tp-TDL.lisp
+++ Tp-TDL.lisp
@@ -1,58 +1,80 @@
 (defvar *global-env* (make-hash-table :test 'equal))
-(defvar locallist (list nil))
+(defvar var-reflist nil) ;; Lista de referencias (nombres de variables)
+;; var-reflist es una lista pero pongo las variables como un stack cuando las introduzco
+
+;; Printear valor por pantalla
+(defun exec-browse (val env)
+  (if (symbolp val) ;; Verifica si es un simbolo (en nuestro caso, una variable)
+    (if (find val var-reflist) ;; Si esta en la lista de referencias
+      (print (gethash (find val var-reflist) env)) ;; Imprimo el valor de la variable en el local
+      (error "Variable not introduced: ~a" val)
+    )
+    (print val) ;; Si no, imprime el valor directamente
+  )
+)
+
+;; Operador =
+(defun exec-equality (exp env)
+  (if (symbolp (car exp))
+    (if (find (car exp) var-reflist)
+      (if (gethash (car exp) env)
+        (error "Failed equality constraint: ~a does not equal ~a" (gethash (car exp) env) (caddr exp))
+        (setf (gethash (car exp) env) (caddr exp))
+      )
+      (error "Variable ~a not introduced" (car exp))
+    )
+    (if (= (car exp) (caddr exp))
+      (nil)
+      (error "Failed equality constraint: ~a does not equal ~a" (car exp) (caddr exp))
+    )
+  )
+)
 
 (defun evaluar-oz (exp env)
   (cond
-    ;; Aca evaluo si la expreción es Declare
-    ((eq (car exp) 'declare)
-     (let ((var (cadr exp))
-           (val (caddr exp)))
-       (setf (gethash var env) val)))
+    ;; ;; Aca evaluo si la expreción es Declare
+    ;; ((eq (car exp) 'declare)
+    ;;  (let ((var (cadr exp))
+    ;;        (val (caddr exp)))
+    ;;    (setf (gethash var env) val)))
 
     ;; Aca evaluo si la expreción es Browse
     ;; Imprimir en pantalla usando Browse
-    ((eq (car exp) 'Browse)
-     (let ((val (cadr exp)))
-        (if (symbolp val) ; Verifica si es una variable
-          (if (> (length locallist) 1) ;; si esta el entorno local 
-            (print (gethash (find val locallist) env)) ;; Imprimo el valor de la variable en el local
-            (print (gethash val env))
-          )
-          (print val)
-        ); Si no, imprime el valor directamente
-       nil))
+    (
+      (eq (car exp) 'Browse)
+      (exec-browse (cadr exp) env)
+    )
 
     ;; Otras ramas de la función eval-oz...
     (
       (eq (car exp) 'local)
-      (let ((varname (cadr exp)))
-           (setq locallist (append locallist (list varname)))
-      )
+      (push (cadr exp) var-reflist)
     )
 
+    ;; El = en realidad es más complejo
+    ;; Hay que chequear que no este definido el valor de la variable
+    ;; Descomponer todo lo que esta a la derecha y a la izquierda (puede haber +, -, *, /)
+    ;; (Puede haber otras variables)
     (
       (eq (cadr exp) '=)
-      (let ((var (car exp))
-           (val (caddr exp)))
-           (if (find var locallist)
-             (setf (gethash var env) val)
-           )
-      )
+      (exec-equality exp env)
     )
 
     (
       (eq (car exp) 'end)
-      (if (> (length locallist) 1)
-        (remhash (car (last locallist)) env)
-      )
-      (if (> (length locallist) 1)
-        (setq locallist (remove (car (last locallist)) locallist))
+      (if (> (length var-reflist) 0)
+        (progn (remhash (first var-reflist) env) (pop var-reflist))
       )
     )
-
-    (t (error "Expresión no reconocida: ~a" exp)))
+    
+    (t (error "Expresión no reconocida: ~a" exp))
+  )
 )
 
 (defun run-oz (program)
   (mapcar (lambda (exp) (evaluar-oz exp *global-env*)) program)
+  
+  (if (> (length var-reflist) 0)
+    (error "Expected end")
+  )
 )
--- main.lisp
+++ main.lisp
@@ -1,28 +1,56 @@
 (load "Tp-TDL.lisp")
 
+(print "-----------------------------")
 (run-oz
  '((Browse "Hola mundo")))
 
-(run-oz
- '((declare x 20)
-   (Browse x)))
+;; Esto esta mal, la sintaxis es declare X in, es como local pero open ended
+;; o sea que no tienen scope como local
+;; (print "Declare:")
+;; (run-oz
+;;  '((declare x 20) 
+;;    (Browse x)))
 
+(print "-----------------------------")
 (print "UN LOCAL:")
-
 (
   run-oz '((local A in) (A = 35) (Browse A) (end))
 )
 
-(print "UN LOCAL Y ACCESO AFUERA:")
-
+(print "-----------------------------")
+(print "DOS LOCALS:")
 (
-  run-oz '((local A in) (A = 35) (Browse A) (end) (Browse A))
-  ;; pruebo un browse de A fuera del local pq aca deberia no tener valor
+  run-oz '((local A in) (local B in) (A = 35) (B = 40) (Browse A) (Browse B) (end) (end))
 )
 
-(print "DOS LOCALS Y ACCESO AFUERA:")
+;; Comento estos casos de error porque sino no sigue la ejecucion
 
-(
-  run-oz '((local A in) (local B in) (B = 40) (Browse B) (end) (Browse B) (A = 35) (Browse A) (end) (Browse A))
-  ;; pruebo un browse de A fuera del local pq aca deberia no tener valor
-)
+;; (print "-----------------------------")
+;; (print "EXPECTED END")
+;; (
+;;   run-oz '((local A in) (local B in) (A = 35) (B = 40) (Browse A) (Browse B) (end))
+;; )
+
+;; (print "-----------------------------")
+;; (print "UN LOCAL Y ACCESO AFUERA:")
+;; (run-oz '((local A in) (A = 35) (Browse A) (end) (Browse A))
+;; )
+
+;; (print "-----------------------------")
+;; (print "DOS LOCALS Y ACCESO AFUERA")
+;; (run-oz '((local A in) (local B in) (B = 40) (Browse B) (end) (A = 35) (Browse A) (end) (Browse A))
+;; )
+
+;; (print "-----------------------------")
+;; (print "Variable already bound con =")
+;; (
+;;   run-oz '((local A in) (A = 35) (A = 20) (Browse A) (end))
+;; )
+
+;; (print "-----------------------------")
+;; (print "Equality constraint")
+;; (
+;;   run-oz '((1 = 40))
+;; )
+
+(print "-----------------------------")
